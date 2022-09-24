7286973d7f0c8dafdc8bdd6ee9b354f18bf18692
Thamu Mnyulwa <43708808+ThamuMnyulwa@users.noreply.github.com>
2022-09-24 11:59:00 +0200

    initial folders in place. Probably many mistakes

---
+++ utils.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import sklearn.datasets as datasets
+import matplotlib.pyplot as plt
+import seaborn as sns
+from loguru import logger
+
+
+def load_iris_data():
+    logger.info("Loading iris data with species")
+    iris = datasets.load_iris()
+    df = (
+        pd.DataFrame(
+            data=np.c_[iris["data"], iris["target"]],
+            columns=iris["feature_names"] + ["target"],
+        )
+        .astype({"target": int})
+        .assign(
+            species=lambda x: x["target"].map(dict(enumerate(iris["target_names"])))
+        )
+    )
+    logger.success("Done: Iris data loaded")
+    return df
